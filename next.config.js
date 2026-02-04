2500123eeefc039ae84e2becc7411aa0ec5685bc
alg-bug-engineer <646184101@qq.com>
2026-02-04 17:35:52 +0800

    first

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@ const { withContentlayer } = require('next-contentlayer2')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: [
+    'https://ai-knowledgepoints.cn',
+    'https://www.ai-knowledgepoints.cn',
+    'http://ai-knowledgepoints.cn',
+    'http://www.ai-knowledgepoints.cn',
+  ],
   output: 'standalone',
   images: {
     remotePatterns: [
